ece499037a4c4ec5fd2989da7b7507f4df65c233
Donald Schenck <dschenck@dschenck-OSX.local>
2017-11-08 20:14:46 -0500

    many changes

--- createV1.sh
+++ createV1.sh
@@ -1,5 +1,5 @@
 oc new-build --binary --name=dotnethello
-oc start-build dotnethello --from-dir=. --follow
-oc set probe dc/dotnethello --readiness --get-url=http://5000
+oc start-build dotnethello --from-dir=. --follow 
+oc set probe dc/dotnethello --readiness --get-url=http://:5000
 oc new-app dotnethello
 oc expose service dotnethello
---
+++ curl_port8080.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+
+while true 
+do
+curl --connect-timeout 1 -s http://localhost:8080;
+echo ;
+sleep 1;
+done
